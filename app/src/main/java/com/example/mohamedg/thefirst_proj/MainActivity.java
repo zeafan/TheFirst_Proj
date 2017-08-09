af05b7f34b8f4cb917642cb37012c5f2e4b8ca3a
Mohamed Gomaa Mohamed <mohamedg@ssdev.local>
2017-08-09 13:45:38 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mohamedg/thefirst_proj/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.mohamedg.thefirst_proj;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
